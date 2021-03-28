76fd05a9ad64bbebbef60d43c935575fcfbdfb2e
Mandeep Jaura <Mandeep.jaura@yahoo.ca>
2021-03-27 22:35:24 -0400

    UI pages are added Login, Signup, Indexpage, UserPropertyView

---
+++ PropertyExplorer/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyExplorer.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
