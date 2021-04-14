e37b64d8ff16e437627d34e767ab4ccb52f4ee35
Baljinder Singh <sing8665@sheridancollege.ca>
2021-04-14 03:36:08 -0400

    User Controller File edited

--- PropertyExplorer/Controllers/UserController.cs
+++ PropertyExplorer/Controllers/UserController.cs
@@ -8,13 +8,60 @@ namespace PropertyExplorer.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult SignUp()
+        private IUserRepository userRepository;
+
+        public object FormsAuthentication { get; private set; }
+
+        public UserController(IUserRepository userRepository)
         {
+
+            this.userRepository = userRepository;
+
+        }
+        [HttpPost]
+        public IActionResult SignUp(User user, string RoleName)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+
+
+                userRepository.Add(user);
+                return RedirectToAction("Login");
+
+            }
             return View();
         }
-        public IActionResult Login()
+        [HttpPost]
+        public IActionResult Login(User user, string ReturnUrl)
         {
+            var activeUser = userRepository.GetAnUser(user);
+            if (activeUser != null)
+            {
+                HttpContext.Session.SetString("username", activeUser.Username);
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "userObject", activeUser);
+                if (ReturnUrl != null)
+                {
+                    return Redirect(ReturnUrl);
+                }
+                else
+                {
+                    ViewBag.session = HttpContext.Session.GetString("username");
+                    return Redirect("~/Home/index");
+                }
+
+            }
             return View();
         }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("userObject");
+            HttpContext.Session.Clear();
+            return Redirect("~/Home/Index");
+        }
+
+
     }
 }
