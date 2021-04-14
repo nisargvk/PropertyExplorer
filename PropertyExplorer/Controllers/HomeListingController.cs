0b2901eda531cf84ce08c67b0101ff7de2a62340
Nisarg Khandhar <khandhar@sheridancollege.ca>
2021-04-14 01:57:21 -0400

    Added IHomeListingRepository HomeListing IHomeListingOperations SessionHelper classes

---
+++ PropertyExplorer/Controllers/HomeListingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PropertyExplorer.Models;
+
+namespace PropertyExplorer.Controllers
+{
+    public class HomeListingController
+    {
+        private readonly IHomeListingRepository homeListingRepository;
+
+        public HomeListingController(IHomeListingRepository homeListingRepository)
+        {
+            this.homeListingRepository = homeListingRepository;
+        }
+
+        public IActionResult CreateHomeListing()
+        {
+           // User activeUser = SessionHelper.GetObjectFromJson<User>(HttpContext.Session, "userObject");
+           // ViewBag.userId = activeUser.Id;
+           return View();
+        }
+
+    }
+}
