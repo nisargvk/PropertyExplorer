45977d31382ca235678dee0c20b2e7a1dfc80203
Baljinder Singh <sing8665@sheridancollege.ca>
2021-04-14 03:30:04 -0400

    UserOperations Files Created and added

--- PropertyExplorer/Models/UserOperation.cs
+++ PropertyExplorer/Models/UserOperation.cs
@@ -7,5 +7,25 @@ namespace PropertyExplorer.Models
 {
     public class UserOperation : IUserRepository
     {
+        private RealEstateDBContext context;
+        public UserOperation(RealEstateDBContext context)
+        {
+            this.context = context;
+        }
+        public void Add(User user)
+        {
+            context.user.Add(user);
+            context.SaveChanges();
+        }
+
+        public User GetAnUser(User user)
+        {
+            return context.user.Where(u => u.Username == user.Username && u.UserType == user.UserType).FirstOrDefault();
+
+        }
+        public User GetUserById(int Id)
+        {
+            return context.user.Find(Id);
+        }
     }
 }
