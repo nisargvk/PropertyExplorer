bc5db1dec8d4a813f9cc275151f17439ea86ba6d
Baljinder Singh <sing8665@sheridancollege.ca>
2021-04-14 03:17:43 -0400

    UserRepository and UserViewProperty Created

---
+++ PropertyExplorer/Models/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyExplorer.Models
+{
+    public interface IUserRepository
+    {
+        User GetAnUser(User user);
+        User GetUserById(int Id);
+        void Add(User user);
+    }
+}
