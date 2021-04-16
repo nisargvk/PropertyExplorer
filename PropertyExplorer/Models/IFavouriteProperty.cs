d02e78ce149f35b4005bc639a4d3e8f9c519aa0b
Mandeep Jaura <Mandeep.jaura@yahoo.ca>
2021-04-16 04:14:36 -0400

    IFav'tProperty Interface is added

---
+++ PropertyExplorer/Models/IFavouriteProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyExplorer.Models
+{
+    interface IFavouriteProperty
+    {
+        
+        IEnumerable<FavouriteList> GetAllFavtProperty();
+
+        
+        IEnumerable<FavouriteList> GetByUserId(int userId);
+        
+        void Add(int userId, int propertyId);
+        
+        void Delete(FavouriteList fList);
+    }
+}
