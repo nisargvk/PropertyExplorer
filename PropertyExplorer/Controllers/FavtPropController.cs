8530e86622b2a7334a4b79017da8841b921117de
Mandeep Jaura <Mandeep.jaura@yahoo.ca>
2021-04-16 04:26:17 -0400

    Fav'tList controller is added

---
+++ PropertyExplorer/Controllers/FavtPropController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PropertyExplorer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GropProject3.Controllers
+{
+    public class FavtPropController : Controller
+    {
+        private readonly IFavouriteProperty client;
+
+        public IActionResult FavouriteProperties()
+        {
+
+            return View(client.GetAllFavtProperty());
+
+        }
+        public IActionResult AddToList(int propertyId)
+        {
+            User activeUser = SessionHelper.GetObjectFromJson<User>(HttpContext.Session, "userObject");
+            int userId = activeUser.Id;
+            client.Add(userId, propertyId);
+            return RedirectToAction("showMyList");
+        }
+
+        public IActionResult showMyList(int userId)
+        {
+            User activeUser = SessionHelper.GetObjectFromJson<User>(HttpContext.Session, "userObject");
+            userId = activeUser.Id;
+
+            return View(client.GetByUserId(userId));
+        }
+    }
+}
