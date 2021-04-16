3b382a76cde53888c4537f1ad0deccc909aeb7ec
Mandeep Jaura <Mandeep.jaura@yahoo.ca>
2021-04-16 04:12:16 -0400

    Favourite List class Added

---
+++ PropertyExplorer/Models/FavouriteList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyExplorer.Models
+{
+    public class FavouriteList
+    {
+        public int Id { get; set; }
+        public Nullable<int> UserId { get; set; }
+        public Nullable<int> HomeListingID { get; set; }
+    }
+}
